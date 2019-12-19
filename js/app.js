582185fd18370a1167d2dec47541cfe38339f29f
Marco Rossi Paccani <marco.rossipaccani@estrobit.com>
2019-12-19 18:47:40 +0100

    feat(index): add landing page with sections, add icons (flaticons, fontawesome), add twitter buttons and feed

--- js/app.js
+++ js/app.js
@@ -1,23 +1,35 @@
+//init svgeezy
+svgeezy.init(false, 'png');
+
+//init timer
 const second = 1000,
       minute = second * 60,
       hour = minute * 60,
       day = hour * 24;
 
 let countDown = new Date('Apr 18, 2020 09:30:00').getTime(),
-    x = setInterval(function() {
+  x = setInterval(function() {
 
-        let now = new Date().getTime(),
-          distance = countDown - now;
+      let now = new Date().getTime(),
+        distance = countDown - now;
 
-        document.getElementById('days').innerText = Math.floor(distance / (day)),
-        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-      
-      //do something later when date is reached
-      //if (distance < 0) {
-      //  clearInterval(x);
-      //  'IT'S MY BIRTHDAY!;
-      //}
+      document.getElementById('days').innerText = Math.floor(distance / (day)),
+      document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+      document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+      document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+    
+    //do something later when date is reached
+    //if (distance < 0) {
+    //  clearInterval(x);
+    //  'IT'S MY BIRTHDAY!;
+    //}
+}, second)
 
-    }, second)
+//init slick
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  prevArrow: '<button type="button" class="slick-prev fas fa-chevron-left">Previous</button>',
+  nextArrow: '<button type="button" class="slick-next fas fa-chevron-right">Next</button>'
+});
